00a72a86d983b48014596a24c61cc9a508d6104c
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 23:40:38 +1000

    Create custom service in AngularJS -Version1

--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -3,9 +3,19 @@
 
 var myApp = angular
             .module("myModule", [])
-            .controller("myController", function ($scope,$http) {
-                $http.get('EmployeeService.asmx/GetAllEmployees')
-                .then(function (res) {
-                    $scope.employees = res.data;
-                });
+            .controller("myController", function ($scope) {
+                $scope.transformString = function (input){
+                    if (!input) {
+                        return input;
+                    }
+                    var output = "";
+                    for (var i = 0; i < input.length; i++) {
+                        if (i>0 && input[i] == input[i].toUpperCase()) {
+                            output += " ";
+                        }
+                        output += input[i];
+                    }
+
+                    $scope.output = output;
+                }
             });
