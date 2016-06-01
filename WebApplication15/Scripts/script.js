f749ea73bf30875ce56a02e230f5795bc7ba64db
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 15:16:22 +1000

    Angular Tutorial - ng-src directive

---
+++ WebApplication15/Scripts/script.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="angular.min.js" />
+
+//var myApp = angular
+//            .module("myModule", [])
+//            .controller("myController", function ($scope) {
+//                var employee = {
+//                    firstName: "Cayden",
+//                    lastName: "Li",
+//                    gender: "Male"
+//                }
+//                $scope.message = "AngularJS Tutorial";
+//                $scope.employee = employee;
+//            });
+
+var myApp = angular
+            .module("myModule", [])
+            .controller("myController", function ($scope) {
+                var pic = {
+                    name: "TestPic",
+                    img: "/img/testimg.jpg"
+                };
+                $scope.pic = pic;
+            });
