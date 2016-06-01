23f08cbf505c8e0a04a4ccb6fe128c24ed98ed99
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 18:43:03 +1000

    AngularJS sort rows by table header

--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -14,4 +14,17 @@ var myApp = angular
 
                 $scope.employees = employees;
                 $scope.sortColumn = "name";
+                $scope.reverseSort = false;
+
+                $scope.sortData = function (column) {
+                    $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+                    $scope.sortColumn = column;
+                };
+
+                $scope.getSortClass = function (column) {
+                    if ($scope.sortColumn == column) {
+                        return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+                    }
+                    return '';
+                }
             });
