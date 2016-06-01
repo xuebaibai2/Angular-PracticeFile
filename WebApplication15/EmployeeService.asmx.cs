d550e762b3155a28020a1acf3b1bd58a60b0e3b8
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 22:34:49 +1000

    Consuming ASP NET Web Service in AngularJS using $http

---
+++ WebApplication15/EmployeeService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+
+namespace WebApplication15
+{
+    /// <summary>
+    /// Summary description for EmployeeService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class EmployeeService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string cs = ConfigurationManager.ConnectionStrings["PracticeDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employees", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee em = new Employee();
+                    em.id = Convert.ToInt32(rdr["Id"]);
+                    em.name = Convert.ToString(rdr["Name"]);
+                    em.gender = Convert.ToString(rdr["Gender"]);
+                    em.salary = Convert.ToInt32(rdr["Salary"]);
+                    listEmployees.Add(em);
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            Context.Response.Write(js.Serialize(listEmployees));
+        }
+    }
+}
--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -3,15 +3,9 @@
 
 var myApp = angular
             .module("myModule", [])
-            .controller("myController", function ($scope) {
-                var employees = [
-                    { name: "Cayden", dateOfBirth: new Date("July 24, 1990"), gender: "Male", salary: 90000.66, city: "Sydney" },
-                    { name: "Lee", dateOfBirth: new Date("June 24, 1990"), gender: "Male", salary: 9453.66, city: "safdblsb" },
-                    { name: "Krystal", dateOfBirth: new Date("April 24, 1990"), gender: "Male", salary: 4864.66, city: "sdjogh" },
-                    { name: "Mike", dateOfBirth: new Date("December 24, 1990"), gender: "Female", salary: 4000.66, city: "hwe4rt" },
-                    { name: "Jane", dateOfBirth: new Date("January 24, 1990"), gender: "Female", salary: 70000.66, city: "ikpoojvb" },
-                ];
-
-                $scope.employees = employees;
-                $scope.employeeView = "employeeTable.html";
+            .controller("myController", function ($scope,$http) {
+                $http.get('EmployeeService.asmx/GetAllEmployees')
+                .then(function (res) {
+                    $scope.employees = res.data;
+                });
             });
