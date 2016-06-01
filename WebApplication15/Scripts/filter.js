b13c2c3fa21dfcfb34481c7e2bc4827d96369993
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 21:43:03 +1000

    ng hide and ng show in AngularJS

--- WebApplication15/Scripts/filter.js
+++ WebApplication15/Scripts/filter.js
@@ -1,14 +1,14 @@
-﻿/// <reference path="script.js" />
+﻿///// <reference path="script.js" />
 
-myApp.filter("gender", function () {
-    return function (gender) {
-        switch (gender) {
-            case 1:
-                return "Male";
-            case 2:
-                return "Female";
-            case 3:
-                return "Not disclosed";
-        }
-    }
-})
+//myApp.filter("gender", function () {
+//    return function (gender) {
+//        switch (gender) {
+//            case 1:
+//                return "Male";
+//            case 2:
+//                return "Female";
+//            case 3:
+//                return "Not disclosed";
+//        }
+//    }
+//})
--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -5,16 +5,12 @@ var myApp = angular
             .module("myModule", [])
             .controller("myController", function ($scope) {
                 var employees = [
-                    { name: "Cayden", dateOfBirth: new Date("July 24, 1990"), gender: 1, salary: 90000.66, city: "Sydney" },
-                    { name: "Lee", dateOfBirth: new Date("June 24, 1990"), gender: 1, salary: 9453.66, city: "safdblsb" },
-                    { name: "Krystal", dateOfBirth: new Date("April 24, 1990"), gender: 3, salary: 4864.66, city: "sdjogh" },
-                    { name: "Mike", dateOfBirth: new Date("December 24, 1990"), gender: 2, salary: 4000.66, city: "hwe4rt" },
-                    { name: "Jane", dateOfBirth: new Date("January 24, 1990"), gender: 2, salary: 70000.66, city: "ikpoojvb" },
+                    { name: "Cayden", dateOfBirth: new Date("July 24, 1990"), gender: "Male", salary: 90000.66, city: "Sydney" },
+                    { name: "Lee", dateOfBirth: new Date("June 24, 1990"), gender: "Male", salary: 9453.66, city: "safdblsb" },
+                    { name: "Krystal", dateOfBirth: new Date("April 24, 1990"), gender: "Male", salary: 4864.66, city: "sdjogh" },
+                    { name: "Mike", dateOfBirth: new Date("December 24, 1990"), gender: "Female", salary: 4000.66, city: "hwe4rt" },
+                    { name: "Jane", dateOfBirth: new Date("January 24, 1990"), gender: "Female", salary: 70000.66, city: "ikpoojvb" },
                 ];
 
                 $scope.employees = employees;
-
-
-
-
             });
