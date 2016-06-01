f58688b16018c42ee497800e4cbcc7a9cc163c7f
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 21:31:43 +1000

    Create a custom filter in AngularJS -Version1

--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -3,28 +3,30 @@
 
 var myApp = angular
             .module("myModule", [])
+            .filter("gender", function () {
+                return function (gender) {
+                    switch (gender) {
+                        case 1:
+                            return "Male";
+                        case 2:
+                            return "Female";
+                        case 3:
+                            return "Not disclosed";
+                    }
+                }
+            })
             .controller("myController", function ($scope) {
                 var employees = [
-                    { name: "Cayden", dateOfBirth: new Date("July 24, 1990"), gender: "Male", salary: 90000.66, city: "Sydney" },
-                    { name: "Lee", dateOfBirth: new Date("June 24, 1990"), gender: "Male", salary: 9453.66, city: "safdblsb" },
-                    { name: "Krystal", dateOfBirth: new Date("April 24, 1990"), gender: "Male", salary: 4864.66, city: "sdjogh" },
-                    { name: "Mike", dateOfBirth: new Date("December 24, 1990"), gender: "Female", salary: 4000.66, city: "hwe4rt" },
-                    { name: "Jane", dateOfBirth: new Date("January 24, 1990"), gender: "Female", salary: 70000.66, city: "ikpoojvb" },
+                    { name: "Cayden", dateOfBirth: new Date("July 24, 1990"), gender: 1, salary: 90000.66, city: "Sydney" },
+                    { name: "Lee", dateOfBirth: new Date("June 24, 1990"), gender: 1, salary: 9453.66, city: "safdblsb" },
+                    { name: "Krystal", dateOfBirth: new Date("April 24, 1990"), gender: 3, salary: 4864.66, city: "sdjogh" },
+                    { name: "Mike", dateOfBirth: new Date("December 24, 1990"), gender: 2, salary: 4000.66, city: "hwe4rt" },
+                    { name: "Jane", dateOfBirth: new Date("January 24, 1990"), gender: 2, salary: 70000.66, city: "ikpoojvb" },
                 ];
 
                 $scope.employees = employees;
 
-                $scope.search = function (item) {
-                    if ($scope.searchText == undefined) {
-                        return true;
-                    } else {
-                        if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || 
-                            item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
+
 
 
             });
