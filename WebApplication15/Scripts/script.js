18dfb2c903e4ba26341d619b45dc8643ec612803
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 23:47:03 +1000

    Create custom service in AngularJS -Version1 -- Create new string service file

--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -3,19 +3,8 @@
 
 var myApp = angular
             .module("myModule", [])
-            .controller("myController", function ($scope) {
+            .controller("myController", function ($scope, stringService) {
                 $scope.transformString = function (input){
-                    if (!input) {
-                        return input;
-                    }
-                    var output = "";
-                    for (var i = 0; i < input.length; i++) {
-                        if (i>0 && input[i] == input[i].toUpperCase()) {
-                            output += " ";
-                        }
-                        output += input[i];
-                    }
-
-                    $scope.output = output;
+                    $scope.output = stringService.processString(input);
                 }
             });
