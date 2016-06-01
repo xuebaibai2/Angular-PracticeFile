2ce7404ce149958da630ea4110ea777a927a734f
Yanfeng <xuebaibai2@hotmail.com>
2016-06-01 15:39:03 +1000

    ng repeat directive -version1

--- WebApplication15/Scripts/script.js
+++ WebApplication15/Scripts/script.js
@@ -4,10 +4,12 @@
 var myApp = angular
             .module("myModule", [])
             .controller("myController", function ($scope) {
-                var employee = {
-                    firstName: "Cayden",
-                    lastName: "Li",
-                    gender: "Male"
-                };
-                $scope.employee = employee;
+                var employees = [
+                    { firstName: "Cayden", lastName: "Li", gender: "Male", salary: 55000 },
+                    { firstName: "Sara", lastName: "Sun", gender: "Female", salary: 30000 },
+                    { firstName: "Mike", lastName: "Qian", gender: "Male", salary: 35000 },
+                    { firstName: "Jeff", lastName: "Zhao", gender: "Female", salary: 42000 },
+                    { firstName: "Andrew", lastName: "Wang", gender: "Male", salary: 45000 }
+                ];
+                $scope.employees = employees;
             });
